f4f6e2da9b877febdd9544726544cc4bde5da269
hendos43 <55980174+hendos43@users.noreply.github.com>
2020-03-05 09:19:12 +0000

    Environment Update (Alembics and FBX added)

---
+++ Assets/MaterialToChildren.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialToChildren : MonoBehaviour
+{
+
+    public Material givenMaterial;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ChangeMaterial(givenMaterial);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void ChangeMaterial(Material newMat)
+    {
+        Renderer[] children;
+        children = GetComponentsInChildren<Renderer>();
+        foreach (Renderer rend in children)
+        {
+            var mats = new Material[rend.materials.Length];
+            for (var j = 0; j < rend.materials.Length; j++)
+            {
+                mats[j] = newMat;
+            }
+            rend.materials = mats;
+        }
+    }
+}
